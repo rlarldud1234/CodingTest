4bee8487309c17dd4da247d1c78144c234a5e870
rlarldud1234 <rlarldud6482@naver.com>
2022-04-24 21:45:29 +0900

    음양 더하기

---
+++ Level1/음양더하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ absolutes: [Int], _ signs: [Bool]) -> Int {
+    var sum = 0
+    for i in 0..<signs.count {
+        if signs[i] {
+            sum += absolutes[i]
+        } else {
+            sum -= absolutes[i]
+        }
+    }
+
+    return sum
+}
+
